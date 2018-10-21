655a34c0b7efb794100b32201daa4221d592037f
Murat <mdenizalioglu@gmail.com>
2018-10-21 03:02:28 +0300

    remove unused remove() method

--- src/bolum05/Tree.java
+++ src/bolum05/Tree.java
@@ -27,32 +27,6 @@ public class Tree {
         }
     }
     
-//    void remove(int data) {
-//        Node a = root;
-//        Node b = null;
-//        
-//        while (a.data != data) {
-//            if (data < a.data) {
-//                a = a.left;
-//            } else {
-//                a = a.right;
-//            }
-//        }
-//        while (true) {
-//            if (a.left != null) {
-//                b = a.left.getMaxIterative();
-//            }
-//            if (b == null && a.right != null) {
-//                b = a.right.getMinIterative();
-//            }
-//            if (b == null) {
-//                break;
-//            }
-//            a.data = b.data;
-//            a = b;
-//        }
-//    }
-    
     void remove(int data) {
         Node a = root;
         Node b = null;
